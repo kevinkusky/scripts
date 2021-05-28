040f061187641543fa1e5fe8d1f05c3541d26365
Kevin Kusky <kevinkusky@gmail.com>
2021-05-28 08:56:28 -0700

    cleans some syntax

--- ruby_scripts/ruby_link_replacer.rb
+++ ruby_scripts/ruby_link_replacer.rb
@@ -3,26 +3,25 @@ require 'csv'
 
 # Must Call in Directory we are Searching in.
 if ARGV.length == 1
-    exts = ['.jsx', '.erb', '.rb', '.js', '.html']
+    file_types = ['.jsx', '.erb', '.rb', '.js', '.html']
+
     search_files = [].tap do |arr|
-        exts.each { |file_type| arr << Dir["**/*#{file_type}"] }
+        file_types.each { |type| arr << Dir["**/*#{type}"] }
     end.uniq
 
-    bad_links =[]
+    bad_links = []
 
-    links_file = CSV.read(ARGV[0]).each { |row| bad_links << row[0] }
+    CSV.read(ARGV[0]).each { |row| bad_links << row[0] }
+    # links_file = CSV.read(ARGV[0]).each { |row| bad_links << row[0] }
 
     bad_links.uniq.each do |bad_url|
-       
-        # Listed items to ignore
         next if bad_url.include?('player.vimeo.com')
 
-        # Flattened Files array
         search_files.flatten.each do |file|
             # conditional check for string inclusion in each file
             if File.open(file).read.include?(bad_url)
-                puts file
-                puts bad_url
+                puts "Current File: #{file}; Current Url: #{bad_url}"
+
                 if !(bad_url.start_with?('https://www') || bad_url.start_with?('http://www'))
                     good_link = 'https://www.' + bad_url.split("://")[1].to_s + '/'
                     puts "prepend www to URL as #{good_link}"
@@ -36,7 +35,6 @@ if ARGV.length == 1
                 end
             end
         end
-        i += 1
     end
 else
     puts "Please run command in Target Dir with <csv_file_path>"
