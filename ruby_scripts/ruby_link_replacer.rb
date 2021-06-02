8dd7b4e01c0aee5ed161503a3148d8dd4e508242
Kevin Kusky <kevinkusky@gmail.com>
2021-06-02 12:11:48 -0700

    plugs in gsub for url replacement

--- ruby_scripts/ruby_link_replacer.rb
+++ ruby_scripts/ruby_link_replacer.rb
@@ -12,29 +12,37 @@ if ARGV.length == 1
     bad_links = []
 
     CSV.read(ARGV[0]).each { |row| bad_links << row[0] }
-    # links_file = CSV.read(ARGV[0]).each { |row| bad_links << row[0] }
 
     bad_links.uniq.each do |bad_url|
         next if bad_url.include?('player.vimeo.com')
 
         search_files.flatten.each do |file|
             # conditional check for string inclusion in each file
-            if File.open(file).read.include?(bad_url)
+
+            file_contents = File.open(file).read
+
+            if file_contents.include?(bad_url)
                 puts "Current File: #{file}; Current Url: #{bad_url}"
 
                 if !(bad_url.start_with?('https://www') || bad_url.start_with?('http://www'))
-                    good_link = 'https://www.' + bad_url.split("://")[1].to_s + '/'
+                    good_url = 'https://www.' + bad_url.split("://")[1].to_s + '/'
                     puts "prepend www to URL as #{good_link}"
+                    
                     # write to file then gsub then close
-                    # bad_url.gsub(bad_url, good_link)
+                    corrected_content = file_contents.gsub(bad_url, good_url)
+                    File.open(file, "w"){|f| f.puts corrected_content}
                 elsif bad_url.end_with?('.html')
-                    good_link = bad_url + '/'
+                    good_url = bad_url + '/'
                     puts 'append slash char'
+
+                    corrected_content = file_contents.gsub(bad_url, good_url)
+                    File.open(file, "w"){|f| f.puts corrected_content}
                 else
                     puts "Bad Case with #{bad_url} in #{file}"
                 end
             end
         end
+        File.close(file)
     end
 else
     puts "Please run command in Target Dir with <csv_file_path>"
