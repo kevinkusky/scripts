c8dd22bb0558be953a4051bcf5acbdb1ecf70146
Kevin Kusky <kevinkusky@gmail.com>
2021-06-02 13:49:22 -0700

    cleans syntax and whitespace

--- ruby_scripts/ruby_link_replacer.rb
+++ ruby_scripts/ruby_link_replacer.rb
@@ -7,27 +7,26 @@ if ARGV.length == 1
 
     search_files = [].tap do |arr|
         file_types.each { |type| arr << Dir["**/*#{type}"] }
-    end.uniq
+    end
 
-    bad_links = []
-
-    CSV.read(ARGV[0]).each { |row| bad_links << row[0] }
+    bad_links = [].tap do |arr|
+        CSV.read(ARGV[0]).each { |row| arr << row[0] }
+    end
 
     bad_links.uniq.each do |bad_url|
         next if bad_url.include?('player.vimeo.com')
 
-        search_files.flatten.each do |file|
-            # conditional check for string inclusion in each file
-
+        search_files.flatten.uniq.each do |file|
             file_contents = File.open(file).read
-
+            
+            # conditional check for string inclusion in each file
             if file_contents.include?(bad_url)
                 puts "Current File: #{file}; Current Url: #{bad_url}"
 
                 if !(bad_url.start_with?('https://www') || bad_url.start_with?('http://www'))
                     good_url = 'https://www.' + bad_url.split("://")[1].to_s + '/'
-                    puts "prepend www to URL as #{good_link}"
-                    
+                    puts "prepend www to URL as #{good_url}"
+
                     # write to file then gsub then close
                     corrected_content = file_contents.gsub(bad_url, good_url)
                     File.open(file, "w"){|f| f.puts corrected_content}
@@ -41,8 +40,8 @@ if ARGV.length == 1
                     puts "Bad Case with #{bad_url} in #{file}"
                 end
             end
+            File.close(file)
         end
-        File.close(file)
     end
 else
     puts "Please run command in Target Dir with <csv_file_path>"
