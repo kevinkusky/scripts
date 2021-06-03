6dd6f3be4f5023869a6d41ddfbaa1fee55c9fba5
Kevin Kusky <kevinkusky@gmail.com>
2021-06-03 14:01:58 -0700

    un-nests if statements

--- ruby_scripts/ruby_link_replacer.rb
+++ ruby_scripts/ruby_link_replacer.rb
@@ -2,46 +2,44 @@
 require 'csv'
 
 # Must Call in Directory we are Searching in.
-if ARGV.length == 1
-    file_types = ['.jsx', '.erb', '.rb', '.js', '.html']
+puts "Please run command in Target Dir with <csv_file_path>" unless ARGV.length == 1
 
-    search_files = [].tap do |arr|
-        file_types.each { |type| arr << Dir["**/*#{type}"] }
-    end
+file_types = ['.jsx', '.erb', '.rb', '.js', '.html']
 
-    bad_links = [].tap do |arr|
-        CSV.read(ARGV[0]).each { |row| arr << row[0] }
-    end
+search_files = [].tap do |arr|
+    file_types.each { |type| arr << Dir["**/*#{type}"] }
+end
 
-    bad_links.uniq.each do |bad_url|
-        next if bad_url.include?('player.vimeo.com')
+bad_links = [].tap do |arr|
+    CSV.read(ARGV[0]).each { |row| arr << row[0] }
+end
 
-        search_files.flatten.uniq.each do |file|
-            file_contents = File.open(file).read
+bad_links.uniq.each do |bad_url|
+    next if bad_url.include?('player.vimeo.com')
+
+    search_files.flatten.uniq.each do |file|
+        file_contents = File.open(file).read
+        
+        # conditional check for string inclusion in each file
+        next unless file_contents.include?(bad_url)
+
+            puts "Current File: #{file}; Current Url: #{bad_url}"
+
+        if !(bad_url.start_with?('https://www') || bad_url.start_with?('http://www'))
+            good_url = 'https://www.' + bad_url.split("://")[1].to_s
             
-            # conditional check for string inclusion in each file
-            if file_contents.include?(bad_url)
-                puts "Current File: #{file}; Current Url: #{bad_url}"
-
-                if !(bad_url.start_with?('https://www') || bad_url.start_with?('http://www'))
-                    good_url = 'https://www.' + bad_url.split("://")[1].to_s
-                    
-                    # write to file then gsub then close
-                    corrected_content = file_contents.gsub(bad_url, good_url)
-                    File.open(file, "w"){|f| f.puts corrected_content}
-                    puts "write to #{file} after gsub"
-                elsif bad_url.end_with?('.html')
-                    good_url = bad_url + '/'
-                    
-                    corrected_content = file_contents.gsub(bad_url, good_url)
-                    File.open(file, "w"){|f| f.puts corrected_content}
-                    puts "write to #{file} after gsub"
-                else
-                    puts "Bad Case with #{bad_url} in #{file}"
-                end
-            end
+            # write to file then gsub then close
+            corrected_content = file_contents.gsub(bad_url, good_url)
+            File.open(file, "w"){|f| f.puts corrected_content}
+            puts "write to #{file} after gsub"
+        elsif bad_url.end_with?('.html')
+            good_url = bad_url + '/'
+            
+            corrected_content = file_contents.gsub(bad_url, good_url)
+            File.open(file, "w"){|f| f.puts corrected_content}
+            puts "write to #{file} after gsub"
+        else
+            puts "Bad Case with #{bad_url} in #{file}"
         end
     end
-else
-    puts "Please run command in Target Dir with <csv_file_path>"
 end
